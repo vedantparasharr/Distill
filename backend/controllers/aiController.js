625185a2f742c806e827f5d4fc10234e0435c24a
vedantparasharr <iemvedant@gmail.com>
2026-03-03 15:24:11 +0530

    feat: enhance flashcard generation with document validation and error handling

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -2,7 +2,7 @@ import Document from "../models/Document.js";
 import Flashcard from "../models/Flashcard.js";
 import Quiz from "../models/Quiz.js";
 import ChatHistory from "../models/ChatHistory.js";
-import * as geminiServie from "../utils/geminiService.js";
+import * as geminiService from "../utils/geminiService.js";
 import { findRelevantChunks } from "../utils/textChunker.js";
 
 // @desc
@@ -10,6 +10,52 @@ import { findRelevantChunks } from "../utils/textChunker.js";
 // @access
 export const generateFlashcards = async (req, res, next) => {
   try {
+    const { documentId, count = 10 } = req.body;
+    if (!documentId) {
+      return res.status(404).json({
+        success: false,
+        error: "Please provide document Id",
+        statusCode: 404,
+      });
+    }
+
+    const document = await Document.findOne({
+      _id: documentId,
+      userId: req.user._id,
+      status: "ready",
+    });
+
+    if (!document) {
+      return res.status(404).json({
+        success: false,
+        error: "Ducument does not exist or not ready",
+        statusCode: 404,
+      });
+    }
+
+    const cards = await geminiService.generateFlashcards(
+      document.extractedText,
+      parseInt(count),
+    );
+
+    // Save to databse
+    const flashcardSet = await Flashcard.create({
+      userId: req.user._id,
+      documentId: document._id,
+      cards: cards.map((card) => ({
+        question: card.question,
+        answer: card.answer,
+        difficulty: card.difficulty,
+        isStarred: false,
+        reviewCount: 0,
+      })),
+    });
+
+    res.status(200).json({
+      success: true,
+      data: flashcardSet,
+      message: "Flashcards generated succesffully",
+    });
   } catch (error) {
     next(error);
   }
