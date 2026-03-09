1d45573913805940bff29800cb8edd857ce99a42
vedantparasharr <iemvedant@gmail.com>
2026-03-09 18:34:30 +0530

    feat: implement services for AI features, document management, and user authentication

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPaths.js";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 80000,
+  withCredentials: true, 
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 500) {
+        console.error("Server error. Please try again later");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout. Please try again");
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
